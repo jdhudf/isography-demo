85212675f00b3c76ea64b8e43a8327847d27782c
OTQStudio <abc.xx0701@gmail.com>
2021-03-26 21:03:11 +0900

    Update toolspanel.js

--- src/components/atelier/toolspanel.js
+++ src/components/atelier/toolspanel.js
@@ -34,6 +34,100 @@ class ToolsPanel extends React.Component {
     }
   }
 
+  handleElement = (action) => {
+
+    const { artboards, working, switchDarkmode,updateArtboard } = this.props
+
+    //console.log(artboards.artboards)
+    let artboard,svg_data,el,data_copy;
+    console.log(artboards.artboards,working)
+
+    for(var i=0;i<artboards.artboards.length;i++){
+      console.log(artboards.artboards[i].artboard_id,working,artboards.artboards[i].artboard_id === working)
+      if(artboards.artboards[i].artboard_id === working) {
+        console.log(artboards.artboards[i])
+        artboard = artboards.artboards[i]
+        data_copy = artboards.artboards[i].canvas.svg_data.slice();
+        el = artboard.canvas.svg_data[this.props.selectEl];
+      }
+    }
+
+
+    switch (action){
+      case 'Duplicate':
+        console.log('duplicate');
+        data_copy.push(el);
+        break;
+      case 'Delete':
+        console.log('delete');
+        data_copy.splice(this.props.selectEl,1);
+        break;
+      case 'Reflect':
+        const regExp = /-?\d+/g;
+        const scale = el.match(regExp)
+
+        var n = 3;
+
+        const result = el.replace(regExp,
+          function(match) {
+            if(n === 3) {
+              n--;
+              return scale[0];
+            } else if (n === 2) {
+              n--;
+              return scale[1];
+            } else if (n === 1) {
+              n--;
+              return -scale[2];
+            } else if (n === 0) {
+              n--;
+              return scale[3];
+            } else {
+              return match;
+            };
+          }
+        );
+
+        data_copy.splice(this.state.selectedElement,1);
+        data_copy.push(result);
+        break;
+      case 'bringToFront':
+        console.log('bringToFront');
+        data_copy.splice(this.props.selectEl,1);
+        data_copy.push(el);
+        break;
+      case 'bringForward':
+        console.log('bringForward');
+        data_copy.splice(this.props.selectEl,1);
+        data_copy.splice(this.props.selectEl + 1 ,0,el);
+        this.props.bringForward();
+        break;
+      case 'sendBackward':
+        console.log('sendBackward');
+        data_copy.splice(this.props.selectEl,1);
+        data_copy.splice(this.props.selectEl - 1 ,0,el);
+        this.props.sendBackward();
+        break;
+      case 'sendToBack':
+        console.log('sendToBack');
+        data_copy.splice(this.props.selectEl,1);
+        data_copy.unshift(el);
+        break;
+      default:
+        break;
+    }
+
+    const newData = updateArtboards({
+      working: working,
+      type: "svg_data",
+      artboards: artboards.artboards,
+      value: data_copy
+    })
+
+    updateArtboard(newData)
+
+  }
+
   removeElement = (props) => {
 
     const { artboards, working, switchDarkmode,updateArtboard } = this.props
@@ -75,7 +169,7 @@ class ToolsPanel extends React.Component {
           if(this.props.selectEl === 0){
             return (
               <span>
-                <p title="Bring Forward" onClick={this.props.bringForward}><FontAwesomeIcon icon={faSortAmountUp} /></p>
+                <p title="Bring Forward" onClick={()=>this.handleElement("bringForward")}><FontAwesomeIcon icon={faSortAmountUp} /></p>
                 <p title="Send Backward" style={{color: "lightgray"}}><FontAwesomeIcon icon={faSortAmountDown} /></p>
               </span>
             )
@@ -83,14 +177,14 @@ class ToolsPanel extends React.Component {
             return (
               <span>
                 <p title="Bring Forward" style={{color: "lightgray"}}><FontAwesomeIcon icon={faSortAmountUp} /></p>
-                <p title="Send Backward" onClick={this.props.sendBackward}><FontAwesomeIcon icon={faSortAmountDown} /></p>
+                <p title="Send Backward" onClick={()=>this.handleElement("sendBackward")}><FontAwesomeIcon icon={faSortAmountDown} /></p>
               </span>
             )
           } else {
             return (
               <span>
-                <p title="Bring Forward" onClick={this.props.bringForward}><FontAwesomeIcon icon={faSortAmountUp} /></p>
-                <p title="Send Backward" onClick={this.props.sendBackward}><FontAwesomeIcon icon={faSortAmountDown} /></p>
+                <p title="Bring Forward" onClick={()=>this.handleElement("bringForward")}><FontAwesomeIcon icon={faSortAmountUp} /></p>
+                <p title="Send Backward" onClick={()=>this.handleElement("sendBackward")}><FontAwesomeIcon icon={faSortAmountDown} /></p>
               </span>
             )
           }
