b3cdb58dfb409786419f086932b3ad872f99f235
OTQStudio <abc.xx0701@gmail.com>
2021-04-19 18:55:53 +0900

    Update images.js

--- src/components/atelier/images.js
+++ src/components/atelier/images.js
@@ -11,6 +11,9 @@ import Img8 from '../../images/materials/pc.inline.svg';
 import Img9 from '../../images/materials/keyboard.inline.svg';
 import Img10 from '../../images/materials/tablet.inline.svg';
 import Img11 from '../../images/materials/mobile.inline.svg';
+import Img12 from '../../images/materials/tablet_vertical.inline.svg';
+import Img13 from '../../images/materials/mobile_vertical.inline.svg';
+import Img14 from '../../images/materials/editor.inline.svg';
 
 class Myloop extends Component {
 
@@ -29,9 +32,9 @@ class Myloop extends Component {
         <div className="item"><Img9/></div>
         <div className="item"><Img10/></div>
         <div className="item"><Img11/></div>
-        <div className="item"><Img1/></div>
-        <div className="item"><Img2/></div>
-        <div className="item"><Img3/></div>
+        <div className="item"><Img12/></div>
+        <div className="item"><Img13/></div>
+        <div className="item"><Img14/></div>
         <div className="item"><Img4/></div>
         <div className="item"><Img5/></div>
         <div className="item"><Img6/></div>
