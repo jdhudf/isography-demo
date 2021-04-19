382aab5b242b2ab578859428e16a386553cf65ef
OTQStudio <abc.xx0701@gmail.com>
2021-04-19 14:22:58 +0900

    Update gallarypanel.js

--- src/components/atelier/gallarypanel.js
+++ src/components/atelier/gallarypanel.js
@@ -48,6 +48,7 @@ class GallaryPanel extends React.Component {
     let gtag = document.createElement('g');
 
     gtag.setAttribute('transform', 'translate(0,0) scale(1.00,1.00)');
+    gtag.setAttribute('data-type', 'el');
 
     for (let i = 0; i< children.length; i++) {
       gtag.appendChild(children[i].cloneNode(true));
